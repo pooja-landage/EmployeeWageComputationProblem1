d3f9aaffe70dd77bd259494c7075aced3e9ef858
pooja-landage <poojalandage22@gmail.com>
2021-11-18 18:50:40 +0530

    [Pooja] : Added UC1 branch

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -6,8 +6,19 @@ namespace Employee
     {
         static void Main(string[] args)
         {
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+
+            int empCheck = random.Next(0,2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("\t Employee is Present...");
+            }
+            else
+            {
+                Console.WriteLine("\t Employee is Absent...");
+            }
             
-            Console.WriteLine("\t Welcome to EmployeeWageComputationProblem!!!!!");
         }
 
     }
