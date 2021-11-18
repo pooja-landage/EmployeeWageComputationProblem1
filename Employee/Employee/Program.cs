a770fb466637b018f9358ed88e10a82441dee303
pooja-landage <poojalandage22@gmail.com>
2021-11-18 17:39:40 +0530

    [Pooja]:Added Program.cs file

---
+++ Employee/Employee/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Employee
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("\t Welcome to EmployeeWageComputationProblem!!!!!");
+        }
+
+    }
+}
+
