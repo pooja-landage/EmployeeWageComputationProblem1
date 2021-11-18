ceb5891e9a1acf56e23c50d4c73c835fd98d424b
pooja-landage <poojalandage22@gmail.com>
2021-11-18 19:18:08 +0530

    [Pooja] : Added UC3 branch

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -6,20 +6,25 @@ namespace Employee
     {
         static void Main(string[] args)
         {
-            int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 1;
+            int IS_FULL_TIME = 2;
             int EMP_RATE_PER_HOUR = 20;
             int empHrs = 0;
             int empWages = 0;
             Random random = new Random();
 
-            int empCheck = random.Next(0,2);
-            if (empCheck == IS_FULL_TIME)
+            int empCheck = random.Next(0, 3);
+            if (empCheck == IS_PART_TIME)
             {
-                empHrs = 8;
+                empHrs = 4;
+            }
+            else if(empCheck == IS_FULL_TIME)
+            {
+                empHrs = 8;  
             }
             else
             {
-                empHrs = 0;   
+                empHrs = 0;
             }
             empWages = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("\t EmpWages :" +empWages);
