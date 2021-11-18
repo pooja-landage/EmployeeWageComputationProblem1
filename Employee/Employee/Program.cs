3b982a08f0e006ec6884c8e70441216264306dd5
pooja-landage <poojalandage22@gmail.com>
2021-11-18 20:38:08 +0530

    [Pooja] : Added UC5 branch

--- Employee/Employee/Program.cs
+++ Employee/Employee/Program.cs
@@ -7,28 +7,37 @@ namespace Employee
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
+        public const int NUM_OF_WORKING_DAYS = 2;
         static void Main(string[] args)
         {
             
             int empHrs = 0;
             int empWages = 0;
-            Random random = new Random();
-
-            int empCheck = random.Next(0, 3);
-            switch (empCheck)
+            int totalEmpWage = 0;
+            for(int day =0; day < NUM_OF_WORKING_DAYS; day++)
             {
-                case IS_PART_TIME:
-                    empHrs = 4;
-                    break;
-                case IS_FULL_TIME:
-                    empHrs = 8;
-                    break;
-                default:
-                    empHrs = 0;
-                    break;
+                Random random = new Random();
+
+                int empCheck = random.Next(0, 3);
+                switch (empCheck)
+                {
+                    case IS_PART_TIME:
+                        empHrs = 4;
+                        break;
+                    case IS_FULL_TIME:
+                        empHrs = 8;
+                        break;
+                    default:
+                        empHrs = 0;
+                        break;
+                }
+                empWages = empHrs * EMP_RATE_PER_HOUR;
+                totalEmpWage += empWages;
+                Console.WriteLine("\t EmpWages :" + empWages);
             }
-            empWages = empHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("\t EmpWages :" +empWages);
+            Console.WriteLine("Total Emp Wage :" +totalEmpWage);
+        
+       
         }
 
     }
